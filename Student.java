f886788145fdbf409a80e6b2b4e40629ad9b3866
RifanMuhammed <rifanmuhammed2028@cs.sjcetpalai.ac.in>
2025-08-01 10:04:47 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,36 @@
 
+class Student{
+	String name;
+	int rollnumber;
+	static int StudentCount;
+	
+	Student(String name,int rollnumber){
+		this.name=name;
+		this.rollnumber=rollnumber;
+		StudentCount++;
+		
+	}
+	public static void displayStudentCount() {
+		System.out.println("student count: "+ StudentCount);	
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Student s1= new Student("Samuel",34);
+		Student.displayStudentCount();
+		
+		Student s2= new Student("Samdauel",314);
+		
+		Student.displayStudentCount();
+		
+		Student s3= new Student("Samufvel",334);
+		
+		Student.displayStudentCount();
+	}
+	
+	
+}
+	
+	
+	
